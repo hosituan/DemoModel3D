db5411296ed0bb4e97d54369dc21f6a3b1d2a772
hosituan <hosituan.work@gmail.com>
2023-03-03 16:18:15 +0700

    Initial Commit

---
+++ 3DModelDemo/_DModelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DModelDemoApp.swift
+//  3DModelDemo
+//
+//  Created by Ho Si Tuan on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct _DModelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
